e879963af57447d451096b51c7f042bbade28b71
Akhil Anand <akhilanandkspa@gmail.com>
2024-10-14 19:14:23 +0000

    Hey this is my third .sql dbt model

---
+++ models/analytics/orders_view.sql
@@ -0,0 +1,9 @@
+Select o.order_id as order_id,
+       c.customer_id as customer_id,
+       c.customer_name as customer_name,
+       p.product_id as Product_id,
+       p.product_name as product_name,
+       c.email_address as email_address
+from raw.orders as o
+Inner Join {{ref("stg_customers")}} as c on o.customer_id=c.customer_id
+inner Join raw.Products as p on o.product_id=p.product_id
